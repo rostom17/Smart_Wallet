b0a86f19874e5845876a49130a2f575d23d616c6
rostom17 <rostom825@gmail.com>
2025-07-18 21:43:59 +0600

    splash_screen done

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,13 +1,22 @@
 import 'package:go_router/go_router.dart';
 import 'package:smart_wallet/features/bottom_nav/presentation/screens/bottom_nav_screen.dart';
+import 'package:smart_wallet/features/splash_screen/presentation/splash_screen.dart';
 
 class AppRoutes {
+  static const String splashScreen = "SplashScreen";
   static const String bottomNavScreen = "BottomNavScreen";
 
   static GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
         path: '/',
+        name: splashScreen,
+        builder: (context, state) {
+          return SplashScreen();
+        },
+      ),
+      GoRoute(
+        path: '/bottomNavScreen',
         name: bottomNavScreen,
         builder: (context, state) {
           return BottomNavScreen();
---
+++ lib/features/splash_screen/presentation/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:smart_wallet/config/app_routes.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 1));
+    if (mounted) {
+      context.pushNamed(AppRoutes.bottomNavScreen);
+    }
+  }
+
+  @override
+  void initState() {
+    _moveToNextScreen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: SvgPicture.asset("assets/images/splash_logo.svg")),
+    );
+  }
+}
