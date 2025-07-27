c520f41c183beedf2442ca33699377a71bf08b6d
rostom17 <rostom825@gmail.com>
2025-07-27 15:44:06 +0600

    app constants added

---
+++ lib/core/constans/app_strings.dart
@@ -0,0 +1,43 @@
+class AppStrings {
+  static const _baseIconPath = "assets/icons";
+  static const appleLogo = "$_baseIconPath/apple-logo.png";
+  static const bankLogo1 = "$_baseIconPath/bank.png";
+  static const bankLogo2 = "$_baseIconPath/bank2.png";
+  static const burgerLogo = "$_baseIconPath/burger.png";
+  static const cashLogo = "$_baseIconPath/cash.png";
+  static const cinemaLogo = "$_baseIconPath/cinema.png";
+  static const clothesLogo = "$_baseIconPath/clothes.png";
+  static const creditCardLogo = "$_baseIconPath/credit.png";
+  static const diamondLogo = "$_baseIconPath/diamond.png";
+  static const downloadLogo = "$_baseIconPath/download.png";
+  static const facebookLogo = "$_baseIconPath/facebook.png";
+  static const fiverrLogo = "$_baseIconPath/fiverr.png";
+  static const googleLogo = "$_baseIconPath/google.png";
+  static const passwordHideLogo = "$_baseIconPath/hide_pass.png";
+  static const homeFillLogo = "$_baseIconPath/home_fill.png";
+  static const homeLogo = "$_baseIconPath/home.png";
+  static const securityLockLogo = "$_baseIconPath/lock.png";
+  static const personLogo = "$_baseIconPath/man.png";
+  static const paypalLogo = "$_baseIconPath/paypal.png";
+  static const pyoneerLogo = "$_baseIconPath/pyoneer.png";
+  static const salaryLogo = "$_baseIconPath/salary.png";
+  static const shieldLogo = "$_baseIconPath/shield.png";
+  static const shoPasswordLogo = "$_baseIconPath/show_pass.png";
+  static const statisticsFillLogo = "$_baseIconPath/stats_fill.png";
+  static const statisticsLogo = "$_baseIconPath/stats.png";
+  static const traveLogo = "$_baseIconPath/travel.png";
+  static const upworkLogo = "$_baseIconPath/upwork.png";
+  static const userFillLogo = "$_baseIconPath/user_fill.png";
+  static const userLogo = "$_baseIconPath/user.png";
+  static const walletFillLogo = "$_baseIconPath/wallet_fill.png";
+  static const walletLogo = "$_baseIconPath/wallet.png";
+  static const youtubeLogo = "$_baseIconPath/youtube.png";
+
+  static const _baseImagePath = "assets/images";
+  static const cardImage = "$_baseImagePath/Cards.png";
+
+  static const chartsSvg = "$_baseImagePath/charts.svg";
+  static const lineSvg = "$_baseImagePath/line.svg";
+  static const appbarPatternSvg = "$_baseImagePath/pattern.svg";
+  static const splashIconSvg = "$_baseImagePath/splash_logo.svg";
+}
