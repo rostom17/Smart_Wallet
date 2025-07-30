ef917dbe09be1bc43f03b83f5261dd3c833078b7
rostom17 <rostom825@gmail.com>
2025-07-30 19:57:27 +0600

    api end point added

---
+++ lib/core/constans/api_constants.dart
@@ -0,0 +1,11 @@
+class ApiConstants {
+  static const baseUrl = "54.90.81.111/api/v1";
+
+  static const loginEndPoint = "/auth/login";
+  static const signUpEndPoint = "/auth/sign-up";
+  static const userProfileEndPoint = "/users/profile";
+
+  static const expensesEndPoint = "/expenses";
+  static const bankCardEndPoint = "/bank-cards";
+  static const backCardTypesEndPoint = "/bank-cards/types";
+}
